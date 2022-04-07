d38c33e68189e31ab6b24647ac7af55be0118f54
hanan m7md <32298658+HananMohammed@users.noreply.github.com>
2022-04-07 06:12:58 +0200

    Fix Call datatble index to upper case

--- resources/js/src/views/dashboard/departments/index.vue
+++ resources/js/src/views/dashboard/departments/index.vue
@@ -1,107 +1,112 @@
 <template>
-    <table-data :titleProp="title" :searchTermProp="searchTerm" :columnsProp="columns" :url="url">
-      <template v-slot:searchDiv>
-            <div class="custom-search d-flex justify-content-end">
-            <b-form-group>
-                <div class="d-flex align-items-center">
-                    <label class="mr-1">Search</label>
-                    <b-form-input
-                        v-model="searchTerm"
-                        placeholder="Search"
-                        type="text"
-                        class="d-inline-block"
-                    />
-                </div>
-            </b-form-group>
-        </div>
+  <table-data
+    :title-prop="title"
+    :search-term-prop="searchTerm"
+    :columns-prop="columns"
+    :url="url"
+  >
+    <template v-slot:searchDiv>
+      <div class="custom-search d-flex justify-content-end">
+        <b-form-group>
+          <div class="d-flex align-items-center">
+            <label class="mr-1">Search</label>
+            <b-form-input
+              v-model="searchTerm"
+              placeholder="Search"
+              type="text"
+              class="d-inline-block"
+            />
+          </div>
+        </b-form-group>
+      </div>
     </template>
 
-    </table-data>
+  </table-data>
 </template>
 
 <script>
-    import  axios from 'axios'
-    import tableData from '../datatable/index'
-        import {
-        BCard,BAvatar, BBadge, BPagination, BFormGroup, BFormInput, BFormSelect, BDropdownItem, BDropdown,BRow, BCol
-    } from 'bootstrap-vue'
+import axios from 'axios'
+import {
+  BCard, BAvatar, BBadge, BPagination, BFormGroup, BFormInput, BFormSelect, BDropdownItem, BDropdown, BRow, BCol,
+} from 'bootstrap-vue'
+import tableData from '../datatable/Index'
 
-    export default {
-        components: {
-            tableData,
-            BCard,
-            BAvatar,
-            BBadge,
-            BPagination,
-            BFormGroup,
-            BFormInput,
-            BFormSelect,
-            // Prism,
-            BDropdownItem,
-            BDropdown,
-            BRow, BCol
+export default {
+  components: {
+    tableData,
+    BCard,
+    BAvatar,
+    BBadge,
+    BPagination,
+    BFormGroup,
+    BFormInput,
+    BFormSelect,
+    // Prism,
+    BDropdownItem,
+    BDropdown,
+    BRow,
+    BCol,
+  },
+  data() {
+    return {
+      url: '/departments',
+      searchTerm: '',
+      title: 'Departments',
+      columns: [
+        {
+          label: 'Name',
+          field: 'name',
+          filterable: true,
+          sortable: true,
         },
-        data() {
-            return {
-                url:'/departments',
-                searchTerm:'',
-                title:'Departments',
-                columns: [
-                    {
-                        label: 'Name',
-                        field: 'name',
-                        filterable:true,
-                        sortable:true
-                    },
-                    {
-                        label: 'Status',
-                        field: 'status',
-                        filterable:true
-
-                    },
-                    {
-                        label: 'Organization',
-                        field: 'organization_id',
-                        filterable:true
-
-                    },
-                    {
-                        label: 'Branch',
-                        field: 'branch_id',
-                        filterable:true
-
-                    },
-                    {
-                        label: 'Created At',
-                        field: 'created_at',
-                        filterable:true
-
-                    },
-                    {
-                        label: 'Phone',
-                        field: 'phone',
-                        filterable:true
-
-                    },
-                    {
-                        label: 'Action',
-                        field: 'action',
-                        sortable:false
-
-                    },
-                ],
-
-            }
-        },
-        computed: {
+        {
+          label: 'Status',
+          field: 'status',
+          filterable: true,
 
         },
-        created() {
+        {
+          label: 'Organization',
+          field: 'organization_id',
+          filterable: true,
 
         },
-        methods: {
+        {
+          label: 'Branch',
+          field: 'branch_id',
+          filterable: true,
 
-        }
+        },
+        {
+          label: 'Created At',
+          field: 'created_at',
+          filterable: true,
+
+        },
+        {
+          label: 'Phone',
+          field: 'phone',
+          filterable: true,
+
+        },
+        {
+          label: 'Action',
+          field: 'action',
+          sortable: false,
+
+        },
+      ],
+
     }
-</script>
+  },
+  computed: {
 
+  },
+  created() {
+
+  },
+  methods: {
+
+  },
+}
+</script>
