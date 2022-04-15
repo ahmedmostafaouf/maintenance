517903c8319b209371db25e4998780d47421e460
mo_moniem2000 <mohamedabdelmoniem101@gmail.com>
2022-04-15 08:54:23 +0300

    permissions edit ;

--- app/Http/Resources/Dashboard/RolesResource.php
+++ app/Http/Resources/Dashboard/RolesResource.php
@@ -14,20 +14,18 @@ class RolesResource extends JsonResource
      */
     public function toArray($request)
     {
-        $all_permissions = '';
-        // if($this->permissions){
-        //     foreach (json_decode($this->permissions) as $key => $value) {
-        //         $read = ($value->read) ? 'Yes' : 'No';
-        //         $write = ($value->read) ? 'Yes' : 'No';
-        //         $edit = ($value->update) ? 'Yes' : 'No';
-        //         $delete = ($value->delete) ? 'Yes' : 'No';
-        //         $all_permissions.=  $value->name .' => [ '. 'Read -> '. $read .' , Write -> '.$write.' , Edit -> '.$edit.' , Delete -> '.$delete.' ] '. "\r\n";
-        //     }
-        // }
+        $permissionsCollection  = collect(json_decode($this->permissions));
+        foreach (permissions() as $permission){
+            $checkPermission = $permissionsCollection->contains('name',$permission);
+            if(!$checkPermission){
+                $item = ['name'=>$permission,'read'=>false,'write'=>false,'update'=>false,'delete'=>false];
+                $permissionsCollection->push((object)$item);
+            }
+        }
         return [
             'id' => $this->id,
             'name' => $this->roleName,
-            'permissions' => json_decode($this->permissions)
+            'permissions' => $permissionsCollection->toArray()
         ];
     }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -32,20 +32,13 @@ class AuthServiceProvider extends ServiceProvider
                 if(!$user->group()->exists())
                     return false;
                 foreach (json_decode($user->group->permissions) as $permission){
-//                    dd(($permission->read ? 'read ' : '') . $permission->name ,$cr_role);
                     if(($permission->read ? 'read ' : '') . $permission->name == $cr_role){
-//                        dd(1);
                         return true;
                     }elseif (($permission->write ? 'write ' : '') . $permission->name == $cr_role){
-//                        dd(2);
-
                         return true;
                     }elseif (($permission->delete ? 'delete ' : '') . $permission->name == $cr_role){
-//                        dd(3);
-
                         return true;
                     }elseif (($permission->update ? 'update ' : '') . $permission->name == $cr_role){
-//                        dd(4);
                         return true;
                     }else{
                        continue;
