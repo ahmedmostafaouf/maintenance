e5270e06785521b17a885bbc65a5fbe1fdd3c986
himasmai <himasami0000@gmail.com>
2022-04-22 15:10:15 -0700

    emoji

--- resources/js/src/views/dashboard/organizations/form.vue
+++ resources/js/src/views/dashboard/organizations/form.vue
@@ -386,7 +386,7 @@
             >
               <b-input-group class="input-group-merge">
                 <b-input-group-prepend is-text>
-                  <feather-icon icon="SidebarIcon" />
+                  <feather-icon icon="SidebarIc on" />
                 </b-input-group-prepend>
                 <b-form-input
                   id="vi-instance_id"
@@ -409,16 +409,52 @@
               label="Welcome Message"
               label-for="vi-temp_msg"
             >
+
+                    <emoji-picker @emoji="append" :search="search">
+                    <div
+                        class="emoji-invoker"
+                        slot="emoji-invoker"
+                        slot-scope="{ events: { click: clickEvent } }"
+                        @click.stop="clickEvent"
+                    >
+                        <svg height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"/>
+                        </svg>
+                    </div>
+                    <div slot="emoji-picker" slot-scope="{ emojis, insert }">
+                        <div class="emoji-picker" :style="{ top: 50 + 'px', right: 16 + 'px' }">
+                        <div class="emoji-picker__search">
+                            <input type="text" v-model="search" v-focus>
+                        </div>
+                        <div>
+                            <div v-for="(emojiGroup, category) in emojis" :key="category">
+                            <h5>{{ category }}</h5>
+                            <div class="emojis">
+                                <span
+                                v-for="(emoji, emojiName) in emojiGroup"
+                                :key="emojiName"
+                                @click="insert(emoji)"
+                                :title="emojiName"
+                                >{{ emoji }}</span>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </emoji-picker>
               <b-input-group class="input-group-merge">
                 <b-input-group-prepend is-text>
                   <feather-icon icon="MessageCircleIcon" />
                 </b-input-group-prepend>
+
                 <b-form-textarea
                   id="vi-temp_msg"
                   v-model="organization.temp_msg"
                   rows="5"
                   type="text"
                   placeholder="Welcome Message"
+                  class="welcome_msg_textarea"
                 />
               </b-input-group>
               <label
@@ -510,6 +546,7 @@
               </label>
             </b-form-group>
           </b-col>
+
         </b-row>
       </tab-content>
     </form-wizard>
@@ -527,10 +564,12 @@ import 'vue-form-wizard/dist/vue-form-wizard.min.css'
 import axios from 'axios'
 import Ripple from 'vue-ripple-directive'
 import vSelect from 'vue-select'
+import { EmojiPicker } from 'vue-emoji-picker'
 
 export default {
   components: {
     BRow,
+    EmojiPicker,
     vSelect,
     BCol,
     BFormValidFeedback,
@@ -553,6 +592,8 @@ export default {
   },
   data() {
     return {
+        input: '',
+      search: '',
       errors: {},
       organization: {
         name: '',
@@ -586,7 +627,17 @@ export default {
       this.getOrganization()
     }
   },
+   directives: {
+    focus: {
+      inserted(el) {
+        el.focus()
+      },
+    },
+  },
   methods: {
+      append(emoji) {
+      this.organization.temp_msg += emoji
+    },
     formSubmitted() {
       const config = {
         headers: {
@@ -676,4 +727,77 @@ export default {
 .v-select{
     width: 100% !important;
 }
+
+
+
+.emoji-invoker {
+position: absolute;
+    top: 25px;
+    z-index: 9;
+    right: 23px;
+  width: 1.5rem;
+  height: 1.5rem;
+  border-radius: 50%;
+  cursor: pointer;
+  transition: all 0.2s;
+}
+.emoji-invoker:hover {
+  transform: scale(1.1);
+}
+.emoji-invoker > svg {
+  fill: #b1c6d0;
+}
+
+.emoji-picker {
+  position: absolute;
+  z-index: 1;
+  font-family: Montserrat;
+  border: 1px solid #161d31;
+  width: 15rem;
+  height: 20rem;
+  overflow-y: scroll;
+  padding: 1rem;
+  box-sizing: border-box;
+  border-radius: 0.5rem;
+  background: #283046 !important;
+
+}
+.emoji-picker__search {
+  display: flex;
+}
+.emoji-picker__search > input {
+  flex: 1;
+  border-radius: 10rem;
+  border: 1px solid #ccc;
+  padding: 0.5rem 1rem;
+  outline: none;
+  margin-bottom: 7px;
+    width: 166px !important;
+}
+.emoji-picker h5 {
+  margin-bottom: 0;
+  color: #b1b1b1;
+  text-transform: uppercase;
+  font-size: 0.8rem;
+  cursor: default;
+}
+.emoji-picker .emojis {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.emoji-picker .emojis:after {
+  content: "";
+  flex: auto;
+}
+.emoji-picker .emojis span {
+  padding: 0.2rem;
+  cursor: pointer;
+  border-radius: 5px;
+}
+.emoji-picker .emojis span:hover {
+  background: #ececec;
+  cursor: pointer;
+}
+
 </style>
